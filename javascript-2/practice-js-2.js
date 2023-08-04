b5e03f9fbc4999a0dba8930e907b464886ca4f8c
Benjamin Cook <benjamincook1313@gmail.com>
2023-08-04 14:20:53 -0600

    finished

---
+++ javascript-2/practice-js-2.js
@@ -0,0 +1,116 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+//CODE HERE
+const gitDefinition = "Local Version Control System"
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+//CODE HERE
+const gitHubDefinition = "Remote Version Control system"
+
+//////////////////PROBLEMS 3 - 9////////////////////
+/*
+    For the next several problems you will be creating objects containing information about different git 
+    commands.  Each object should contain 'description' and 'code' properties.  The 'description' property 
+    will be a string with a description of what that git command does.  The 'code' property should be a 
+    string of the actual command (what you type into your terminal).
+*/
+
+//////////////////PROBLEM 3////////////////////
+/*
+    Create an object called 'init' with 'description' and 'code' properties 
+    following the guidelines above to describe the init command.
+*/
+
+//CODE HERE
+const init = {
+    description: "Initializes a new repository in the currrent directory",
+    code: "git init"
+};
+
+//////////////////PROBLEM 4////////////////////
+/*
+    Create an object called 'clone' with 'description' and 'code' properties 
+    following the guidelines above to describe the clone command.
+*/
+
+//CODE HERE
+
+const clone = {
+    description: "Create a copy of a project down to your machine from a remote location",
+    code: "git clone 'repo url'"
+};
+
+
+//////////////////PROBLEM 5////////////////////
+/*
+    Create an object called 'status' with 'description' and 'code' properties 
+    following the guidelines above to describe the status command.
+*/
+
+//CODE HERE
+const status = {
+    definition: "Creates a snap shot of what changes have been made in my project environment since that last commit",
+    code: "git status"
+}
+
+//////////////////PROBLEM 6////////////////////
+/*
+    Create an object called 'add' with 'description' and 'code' properties 
+    following the guidelines above to describe the add command.
+*/
+
+//CODE HERE
+const add = {
+    description: "adds my changes to the staging area",
+    code: "git add ."
+}
+
+//////////////////PROBLEM 7////////////////////
+/*
+    Create an object called 'commit' with 'description' and 'code' properties
+    following the guidelines above to describe the commit command.
+*/
+
+//CODE HERE
+const commit = {
+    description: "saves the staged changes in the hidden .git file",
+    code: 'git commit -m "commit message"'
+}
+
+//////////////////PROBLEM 8////////////////////
+/*
+    Create an object called 'addRemote' with 'description' and 'code' properties
+    following the guidelines above to describe the command to add a remote location to your git repository.
+*/
+
+//CODE HERE
+const addRemote = {
+    description: "creates a new connection record to a remote repository",
+    code: "git remote 'name of branch'"
+}
+
+//////////////////PROBLEM 9////////////////////
+/*
+    Create an object called 'push' with 'description' and 'code' properties 
+    following the guidelines above to describe the push command.
+*/
+
+//CODE HERE
+const push = {
+    description: "Sends the saved changes from git up to github or another remote versioncontrol system",
+    code: "git push"
+}
